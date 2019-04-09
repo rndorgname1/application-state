312c1acc5413faf485d6d06fff05ee385d2859f5
Daniel Botka <daniel.botka@incepteam.com>
2019-04-09 18:03:36 +0300

    first commit

---
+++ src/main/java/com/ms/applicationstate/ApplicationStateApplication.java
@@ -0,0 +1,13 @@
+package com.ms.applicationstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStateApplication.class, args);
+	}
+
+}
